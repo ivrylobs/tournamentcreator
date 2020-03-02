e12b811f733e58d96cfc9ea1fcaec51746de06a7
Roongpetch Kunthakuldusadee <ivrylobs@Roongpetchs-MacBook-Pro.local>
2020-03-03 02:01:51 +0700

    Create readable database function

--- index.js
+++ index.js
@@ -12,11 +12,31 @@ const userDataSchema = new mongoose.Schema ({
 	password: String
 })
 
+const Username = mongoose.model("UserData", userDataSchema)
+
+
+
 app.use(express.static(__dirname + "/"));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", function(req, res) {
 	res.sendfile(__dirname + "/index.html");
 });
+
+app.post("/", function(req, res) {
+	res.send("Thanks you for registeration")
+	console.log(req.body)
+	const newUser = new Username ( {
+		email: req.body.email,
+		password: req.body.psw
+	})
+	
+	newUser.save()
+})
+
+app.post("/login/", function(req, res) {
+	res.sendfile(__dirname + "/index.html")
+})
 
 app.listen(3000, function() {
 	console.log("Server is running on port 3000");
