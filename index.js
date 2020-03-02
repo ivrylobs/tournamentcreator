8f41a8a762e930d67f2ddc8ece639e982ca30305
Roongpetch Kunthakuldusadee <ivrylobs@Roongpetchs-MacBook-Pro.local>
2020-03-03 00:58:57 +0700

    Create database Scheme

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/colosseumDB", {
+	useNewUrlParser: true
+});
+
+const userDataSchema = new mongoose.Schema ({
+	email: String,
+	password: String
+})
+
 app.use(express.static(__dirname + "/"));
 
 app.get("/", function(req, res) {
