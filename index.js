b373f6dddb554187519f418d4da954f248084601
Roongpetch Kunthakuldusadee <ivrylobs@Roongpetchs-MacBook-Pro.local>
2020-03-03 13:05:32 +0700

    Able to validate username and password

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const userDataSchema = new mongoose.Schema({
 });
 const Username = mongoose.model("UserData", userDataSchema);
 
-
 //App using libraries.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/"));
@@ -33,6 +32,26 @@ app.post("/register", function(req, res) {
 	userAndPsw.save()
 	res.sendFile(__dirname + "/login.html")
 });
+
+app.post("/login", function(req, res) {
+	var userName = req.body
+	
+	Username.find(function(err, users) {
+		if (err) {
+			console.log(err)
+		} else {
+			for(var i = 0; i < users.length; i++) {
+				if(userName.userName == users[i].email && userName.passWord == users[i].password) {
+					console.log("match", userName.userName, users[i].email, userName.passWord, users[i].password)
+					res.redirect("/")
+				} else {
+					console.log("Not Match", userName.userName, users[i].email, userName.passWord, users[i].password)
+					res.sendFile(__dirname + "/login.html")
+				}
+			}
+		}
+	})
+})
 
 app.listen(3000, function() {
 	console.log("Server is running on port 3000");
