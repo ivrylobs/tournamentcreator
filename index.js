088d3498f2d4eefc08d4daeaee6a43848f9b755d
Roongpetch Kunthakuldusadee <ivrylobs@Roongpetchs-MacBook-Pro.local>
2020-03-03 12:33:15 +0700

    Able to push data into database

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
+//JS Libraries import (require)
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
-
 const mongoose = require("mongoose");
+
+const app = express();
+const userDataSchema = new mongoose.Schema({
+	email: String,
+	password: String
+});
+const Username = mongoose.model("UserData", userDataSchema);
+
+
+//App using libraries.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/"));
+
 mongoose.connect("mongodb://localhost:27017/colosseumDB", {
 	useNewUrlParser: true
 });
 
-const userDataSchema = new mongoose.Schema ({
-	email: String,
-	password: String
-})
-
-app.use(express.static(__dirname + "/"));
-
 app.get("/", function(req, res) {
 	res.sendfile(__dirname + "/index.html");
+});
+
+app.post("/register", function(req, res) {
+	var userName = req.body
+	const userAndPsw = new Username({
+		email: userName.email,
+		password: userName.psw
+	})
+
+	userAndPsw.save()
+	res.sendFile(__dirname + "/login.html")
 });
 
 app.listen(3000, function() {
