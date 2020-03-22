28501d12b58460fcc4db903b1b46995327d836d5
Roongpetch Kunthakuldusadee <ivrylobs@Roongpetchs-MacBook-Pro.local>
2020-03-22 07:33:27 +0700

    Created DB Connection to Atlas

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+const adminPwd = "weLMkubRUUCM5k44";
+const cnnStrAdmin =
+	"mongodb+srv://admin-public:" +
+	adminPwd +
+	"@cluster0-4onrp.gcp.mongodb.net/tournamentDB";
+
+mongoose.connect(cnnStrAdmin, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+});
+
+const fruitScheme = new mongoose.Schema({
+	name: String,
+	rating: Number,
+	review: String
+});
+
+const Fruit = mongoose.model("Fruite", fruitScheme);
+
+const fruit = new Fruit({
+	name: "Apple",
+	rating: 7,
+	review: "Pretty solid as a fruit."
+});
+
+fruit.save();
 
 app.listen(process.env.PORT || 3000, function() {
-	console.log("Server is running at port 3000 or dynamically by Heroku Server.")
-})
+	console.log(
+		"Server is running at port 3000 or dynamically by Heroku Server."
+	);
+});
 
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html")
-})
+	res.sendFile(__dirname + "/index.html");
+});
